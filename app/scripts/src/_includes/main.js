35122d0a954066a4c6a09ce61c824c57b5f264f5
Victor Longon <victor.longon@zooma.se>
2015-12-01 09:49:38 +0100

    add js files

---
+++ app/scripts/src/_includes/main.js
@@ -0,0 +1,16 @@
+/**
+ * Victor Longon
+ * @version 0.1
+ * @author www.victorlongon.com
+ * @copyright Victor Longon 2015
+ */
+
+// Doc content loaded
+(function () {
+
+    document.addEventListener("DOMContentLoaded", function() {
+        console.log('document is loaded');
+    });
+
+})();
+
